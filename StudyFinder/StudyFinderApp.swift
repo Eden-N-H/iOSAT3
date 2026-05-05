2fe942d216da13bef3675027c73ffbaa2a8c06b4
Eden-N-H <eden.hallett2005@gmail.com>
2026-05-05 18:12:25 +1000

    added xcode project files

---
+++ StudyFinder/StudyFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyFinderApp.swift
+//  StudyFinder
+//
+//  Created by Eden Hallett on 5/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct StudyFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
